4c16b1f58d137abb25326445aa63c67042dc3e01
Schnitzelreaper <schnitzelreaper@gmail.com>
2022-01-30 12:55:56 +0100

    git init

---
+++ WinnerGetsCards.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+
+namespace WinnerGetsCards
+{
+    [BepInPlugin("com.schnitzelreaper.rounds.WinnerGetsCards", "WinnerGetsCards", "1.0.0")]
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+
+    public class WinnerGetsCards : BaseUnityPlugin
+    {
+        private const string ModId = "ot.dan.rounds.PlayerMarkers";
+        private const string ModName = "Player Markers";
+        public const string Version = "1.1.1";
+        public const string ModInitials = "PM";
+        private const string CompatibilityModName = "PlayerMarkers";
+        public static PlayerMarkers instance { get; private set; }
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin com.schnitzelreaper.rounds.WinnerGetsCards is loaded!");
+        }
+    }
+}
